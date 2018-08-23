f94825039486b80565fb63679c817a75cdca854c
kij <kijmail@gmail.com>
2018-08-24 00:34:14 +0200

    add stupid cache

--- sys/shellcheck.sh
+++ sys/shellcheck.sh
@@ -1,12 +1,31 @@
 #!/bin/sh
 
+
+checkshellscript() {
+	filelist="$1"
+	checkfun="$2"
+
+	while read -r file 
+	do
+		$checkfun "$file"	
+	done < "$filelist"
+		
+}
+
 FMT=gcc
-if [ -n "${SHELLCHECK_XML}" ]; then
-	FMT=checkstyle
-fi
+#if [ -n "${SHELLCHECK_XML}" ]; then
+#	FMT=checkstyle
+#fi
 
 SHCHK="shellcheck --format=${FMT}"
 
-find .  -not -iwholename '*.git*'  | xargs file | grep "POSIX shell script" --line-buffered  | cut -d: -f1 | while read -r file; do ${SHCHK} "$file"; done
+if ! [ -f "./sys/known_scripts" ] ;
+	echo "Find all shellscripts"
+	find .  -not -iwholename '*.git*'  | xargs file | grep "POSIX shell script" | cut -d: -f1  > known_scripts
+fi
+
+checkshellscript "./sys/known_scripts" "$SHCHK"
 
 
+
+ 
